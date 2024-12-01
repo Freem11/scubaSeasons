ff1cb1c34e69a1384636eb5889633eff3744a734
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2024-12-01 08:31:48 -0700

    js media querylib installed, icon importing code migrated fomr web verision

--- src/components/topBar.tsx
+++ src/components/topBar.tsx
@@ -1,37 +1,48 @@
 import './topBar.css'
 import Manta from "../content/Matt_Manta_White.png";
-import FacebookLogo from "../content/facebook.png";
-import InstagramLogo from "../content/instagram.png";
-import YoutubeLogo from "../content/youtube.png"
+import Icon from '../icons/Icon';
+import { useMediaQuery } from 'react-responsive';
 
 function TopBar() {
+  const isMobile = useMediaQuery({ query: '(max-width: 880px)' });
+  const isDesktop = useMediaQuery({ query: '(min-width: 880px)' });
+
+
   return (
     <div className="bodyContainer">
 
       <div className="divLeft">
        <img src={Manta} className="mantalogo"></img>
-      <h2 className="logoname" style={{ fontSize: '3vh', fontWeight: 'lighter', fontStyle: 'italic'}}>
+      <h2 style={{ fontSize: '4vh', fontWeight: 'lighter', fontStyle: 'italic'}}>
         Scuba SEAsons
       </h2>
       </div>
 
-      <div className='divRight'>
-      <a href='https://www.facebook.com/profile.php?id=61554622375177' target="_blank">
-      <img src={FacebookLogo} className="facebookLogo"></img>
-    </a>
+    
 
-    <a href='https://www.instagram.com/scuba_seasons/' target="_blank">
-    <img src={InstagramLogo} className="instagramLogo"></img>
-    </a>
-      
-    <a href='https://www.youtube.com/channel/UCAQzYoPicEgztcfT6r9xc6w' target="_blank">
-    <img src={YoutubeLogo} className="youtubeLogo"></img>
-    </a>
-      
-      <div className="buttonstyler" onClick={() => window.location = 'mailto:scubaseasons@gmail.com'} style={{ fontSize: '3vh', color: 'yellow'}}>
-        Contact Us
-      </div> 
-      </div>
+{isMobile && 
+ <Icon name="menu" className='menuIcon'/>
+ }
+
+ {isDesktop && 
+  <div className='divRight'>
+       <a href='https://www.facebook.com/profile.php?id=61554622375177' target="_blank">
+       <Icon name="facebook" className='facebookLogo'/>
+     </a>
+ 
+     <a href='https://www.instagram.com/scuba_seasons/' target="_blank">
+     <Icon name="instagram" className='instagramLogo'/>
+     </a>
+       
+     <a href='https://www.youtube.com/channel/UCAQzYoPicEgztcfT6r9xc6w' target="_blank">
+     <Icon name="youtube" className='youtubeLogo'/>
+     </a>
+       
+       <div className="buttonstyler" onClick={() => window.location = 'mailto:scubaseasons@gmail.com'}>
+       <Icon name="email-send-outline" className='emailIcon'/>
+       </div> 
+     
+       </div> }
 
   </div>
   );
--- src/components/videoPlayer.tsx
+++ src/components/videoPlayer.tsx
@@ -1,11 +1,13 @@
 import phoneCase from "../content/iPhone14Skin.png";
 import headlinerVid from "../content/headlinerVideo.mp4";
+import { useMediaQuery } from 'react-responsive';
 
 function VideoPlayer() {
-
+  const isMobile = useMediaQuery({ query: '(max-width: 880px)' });
+  const isDesktop = useMediaQuery({ query: '(min-width: 880px)' });
 
   return (
-    <div style={{position: 'relative', display: 'flex', alignItems: "center", justifyContent: 'center'}}>
+    <div style={{position: 'relative', display: 'flex', alignItems: "center", justifyContent: 'center', marginTop: isDesktop ? '20%' : 0}}>
       <img src={phoneCase} style={{position: 'absolute', height: '80vh'}}></img>
       <video src={headlinerVid} style={{ height: '78vh', borderRadius: '4vh'}} autoPlay loop muted playsInline/>
     </div>
