efcca6941741a1a499276ea698c5b12eb2e48a79
Matthew Freeman <matthewfreeman@Matthews-MacBook-Pro.local>
2025-07-29 21:40:31 -0600

    cloudflareMigration: added why question to faq

--- src/components/phoneVideo.tsx
+++ src/components/phoneVideo.tsx
@@ -12,7 +12,7 @@ export default function PhoneVideo() {
 
       <img src={'https://pub-9114df4c0fd044d0806a9e8819aa3212.r2.dev/iPhone16plus.png'} className={styles.phoneCase}></img>
       <video
-        src={'https://pub-9114df4c0fd044d0806a9e8819aa3212.r2.dev/IPhone2025.mp4'}
+        src={'https://pub-9114df4c0fd044d0806a9e8819aa3212.r2.dev/iPhone2025.mp4'}
         className={styles.phoneVideo}
         autoPlay
         loop
@@ -20,8 +20,8 @@ export default function PhoneVideo() {
         playsInline
         preload="auto"
       >
-        <source src={'https://pub-9114df4c0fd044d0806a9e8819aa3212.r2.dev/IPhone2025.mp4'} type="video/mp4" />
-        <source src={'https://pub-9114df4c0fd044d0806a9e8819aa3212.r2.dev/IPhone2025.mp4'} type="video/webm" />
+        <source src={'https://pub-9114df4c0fd044d0806a9e8819aa3212.r2.dev/iPhone2025.mp4'} type="video/mp4" />
+        <source src={'https://pub-9114df4c0fd044d0806a9e8819aa3212.r2.dev/iPhone2025.mp4'} type="video/webm" />
       </video>
     </div>
   );
---
+++ src/pages/faq/sections/WhyAddSite.tsx
@@ -0,0 +1,29 @@
+import styles from "../FAQ.module.css";
+
+export default function WhyAddSite() {
+  return (
+    <div>
+      <div className={styles.faqAnswer}>
+        When we go diving we are often told the <strong>name of a dive site</strong> and record it for our dive logs.
+      </div>
+
+      <div style={{ height: 20 }} />
+
+      <div className={styles.faqAnswer}>
+        In <strong>Scuba SEAsons</strong>, dive sites serve as the <strong>‘anchor’ for sea life sightings</strong>. You know what dive site you were at when you took the photo, and this <strong> makes it easier for divers to make contributions</strong> with their sea life sightings.
+      </div>
+
+      <div style={{ height: 20 }} />
+
+      <div className={styles.faqAnswer}>
+        Adding a sighting to a <strong>dive site by name</strong> is much easier than trying to add it using <strong>GPS coordinates</strong>.
+      </div>
+
+      <div style={{ height: 20 }} />
+
+      <div className={styles.faqAnswer}>
+        If your <strong>local dive sites</strong> are in Scuba SEAsons, <strong>all divers can add their sea life sightings</strong>, and you get to <strong>show off why other divers should come dive in your local area!</strong>
+      </div>
+    </div>
+  );
+}
